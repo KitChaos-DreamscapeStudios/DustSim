128bc57e086f2473c832180541d238a230c392ab
KitChaos <cmeow176@gmail.com>
2024-05-13 10:02:09 -0400

    Added binaries

--- Main.cpp
+++ Main.cpp
@@ -1,47 +1,65 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <map>
 #include <tuple>
 #include <vector>
-struct Vector2
-{
+struct Vector2 {
   int x;
   int y;
+  Vector2(int X, int Y){
+    x = X;
+    y = Y;
+  }
+  Vector2 RelValue(Vector2 Diff){
+    return *new Vector2(x+Diff.x, y+Diff.y);
+  }
 };
-class Pixel{
-    public:std::tuple<Vector2> Positions;
-    public:bool HasGravity=true;
+class Pixel {
+public:
+  Vector2 Position;
+
+
+  bool HasGravity = true;
 };
-class World{
-     public:std::vector<Pixel> Pixels;//Active pixel objects
-     public:std::vector<Vector2> AllWorldPositions;
-     void Tick(){
-        for(Pixel p:Pixels){
-            //Test line
+class World {
+public:
+  std::vector<Pixel> Pixels; // Active pixel objects
+  std::map<Vector2, Pixel> PositionsOfPixels;
+  void Tick() {
+    for (Pixel p : Pixels) {
+        //PositionsOfPixels.at(*new Vector2(p.Position.x, p.Position.y)) = NULL;
+        if(p.HasGravity){
+            if(p.Position.y > 0 && PositionsOfPixels.find( p.Position.RelValue(*new Vector2(0,-1)) )==PositionsOfPixels.end()){
+                p.Position.y-=1;
+            }
         }
-     }
+        PositionsOfPixels.at(*new Vector2(p.Position.x, p.Position.y)) = p;
+    }
+  }
 };
 
-int main()
-{
-   
+int main() {
+    World w = *new World();
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+  
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+    w.Tick();
+    window.clear();
+    for(Pixel p :w.Pixels){
+        sf::CircleShape shape(1.f);
+        shape.setFillColor(sf::Color::Red);
+        shape.setPosition(p.Position.x,p.Position.y);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
         window.draw(shape);
         window.display();
     }
+  
+  }
 
-    return 0;
-   
-
+  return 0;
 }
